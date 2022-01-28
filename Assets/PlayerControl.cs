7bcaa2280008426ce17b7a14a40d27bd504ff6c7
Walyn <walyncurtis@gmail.com>
2022-01-28 15:31:18 -0600

    Adds clicking on ground

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    Camera cam;
+    public LayerMask hittable;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("m1 down");
+            RaycastHit hit;
+            if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, hittable))
+            {
+                Debug.Log("hit ground at "+hit.point);
+            }
         }
     }
 }
