6de44f03f17cc4b8cfffd5a112e51f08593bda5b
Walyn <walyncurtis@gmail.com>
2022-01-28 16:26:17 -0600

    Implements root class

--- Assets/PlantRoot.cs
+++ Assets/PlantRoot.cs
@@ -5,34 +5,38 @@ using UnityEngine;
 public class PlantRoot : MonoBehaviour
 {
     public GameObject prefab;
-    public List<Vector3> starts = new List<Vector3>();
-    public List<Vector3> ends = new List<Vector3>();
+    public List<Root> roots = new List<Root>();
 
-    public void CreateObject(Vector3 start, Vector3 end)
+    public GameObject CreateObject(Vector3 start, Vector3 end)
     {
         GameObject nGo = Instantiate(prefab);
         nGo.transform.position = (start + end)/2;
         nGo.transform.right = end - start;
         nGo.transform.localScale = new Vector3(Vector3.Distance(start, end), 1, 1);
-        starts.Add(start);
-        ends.Add(end);
+        return nGo;
     }
 
     public void CreateNewRoot(Vector3 end)
     {
+        Root parent = null;
         Vector3 start = Vector3.zero;
         float dist = end.magnitude;
-        for (int i = 0; i < starts.Count; ++i)
+        for (int i = 0; i < roots.Count; ++i)
         {
-            Vector3 challenge = ClosestPointOnLineSegment(starts[i], ends[i], end);
+            Root chRoot = roots[i];
+            Vector3 challenge = ClosestPointOnLineSegment(roots[i].start, roots[i].end, end);
             float challengeDist = Vector3.Distance(challenge, end);
             if (challengeDist < dist)
             {
                 dist = challengeDist;
                 start = challenge;
+                parent = chRoot;
             }
         }
-        CreateObject(start, end);
+        GameObject visual = CreateObject(start, end);
+        float distAlong = parent != null ? dist / parent.distance : 0;
+        Root root = new Root(start, end, parent, distAlong, visual);
+        roots.Add(root);
     }
 
     //found on https://stackoverflow.com/questions/3120357/get-closest-point-to-a-line
---
+++ Assets/Root.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Root
+{
+    Root parent;
+    List<Root> children = new List<Root>();
+    float distanceFromParentStart;
+    public float distance;
+    public Vector3 start, end;
+    GameObject visual;
+
+    public Root(Vector3 s, Vector3 e, Root p, float d, GameObject v)
+    {
+        start = s;
+        end = e;
+        parent = p;
+        distanceFromParentStart = d;
+        distance = Vector3.Distance(s, e);
+        visual = v;
+
+    }
+}
