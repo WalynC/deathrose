79fa7ef9970291201fc9407625474b066066b008
Walyn <walyncurtis@gmail.com>
2022-01-29 05:13:44 -0600

    Begins work on alternating game state

---
+++ Assets/GameState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public static GameState instance;
+
+    public GameObject day, night;
+    public Transform seed, rose;
+
+    public void DaytimeBegin()
+    {
+        day.SetActive(true);
+        night.SetActive(false);
+        Vector3 plantPos = RootNetwork.instance.GetClosestRootPoint(rose.position);
+        if (Vector3.Distance(plantPos, rose.position) > 5f) Debug.Log("you lose");
+        seed.position = plantPos;
+    }
+
+    public void NighttimeBegin()
+    {
+        day.SetActive(false);
+        night.SetActive(true);
+        rose.position = seed.position;
+    }
+}
