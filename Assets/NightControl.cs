2ba6e9955f7d883fa7882422755de35385125cd1
Walyn <walyncurtis@gmail.com>
2022-01-29 05:38:11 -0600

    Rose (the capsule) now moves around with the player's input

---
+++ Assets/NightControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightControl : MonoBehaviour
+{
+    public Vector2 lateral = Vector2.zero;
+
+    void Update()
+    {
+        lateral = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+}
---
+++ Assets/RoseMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoseMovement : MonoBehaviour
+{
+    public NightControl input;
+    public CharacterController controller;
+    
+    void FixedUpdate()
+    {
+        controller.Move(new Vector3(input.lateral.x, 0, input.lateral.y) * 10f * Time.deltaTime);
+    }
+}
