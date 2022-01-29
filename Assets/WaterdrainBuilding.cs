2a7ef4a0bf4183e4080815807505b0d3c48b00fb
Walyn <walyncurtis@gmail.com>
2022-01-29 04:55:14 -0600

    Adds Waterdrain building and structure

--- Assets/WaterdrainBuilding.cs
+++ Assets/WaterdrainBuilding.cs
@@ -4,29 +4,46 @@ using UnityEngine;
 
 public class WaterdrainBuilding : Building
 {
-    public LayerMask root, water;
+    public LayerMask rootLayer, water;
     public MeshRenderer preview;
+    public GameObject prefab;
     public Material canAfford, noWater, noAfford;
-    int radius = 3;
+    Root root;
+    float radius = 3;
+    List<Water> waters = new List<Water>();
 
     public override void Build()
     {
-        Debug.Log("build");
+        GameObject obj = Instantiate(prefab);
+        obj.transform.position = position;
+        WaterdrainStructure struc = new WaterdrainStructure();
+        foreach (Water w in waters) struc.waters.Add(w);
+        struc.parent = root;
+        struc.obj = obj;
+        struc.Setup();
     }
 
     public override void Preview()
     {
         buildable = false;
         preview.gameObject.SetActive(false);
+        waters.Clear();
         RaycastHit rootHit;
-        if (Physics.Raycast(CameraController.cam.ScreenPointToRay(Input.mousePosition), out rootHit, Mathf.Infinity, root))
+        if (Physics.Raycast(CameraController.cam.ScreenPointToRay(Input.mousePosition), out rootHit, Mathf.Infinity, rootLayer))
         {
-            position = RootNetwork.instance.GetClosestRootPoint(rootHit.point);
-            preview.gameObject.SetActive(true);
+            position = RootNetwork.instance.GetClosestRootPoint(rootHit.point, out root);
+            preview.gameObject.SetActive(root != null);
             preview.transform.position = position;
             if (PlayerMoney.instance.CanAfford(price))
             {
                 preview.material = canAfford;
+                Collider[] colliders = Physics.OverlapSphere(position, radius, water);
+                foreach (Collider c in colliders)
+                {
+                    Water w = c.GetComponent<Water>();
+                    if (w != null) waters.Add(w);
+                }
+                if (waters.Count == 0) preview.material = noWater;
             } else
             {
                 preview.material = noAfford;
@@ -38,6 +55,8 @@ public class WaterdrainBuilding : Building
     public override void Disable()
     {
         preview.gameObject.SetActive(false);
+        PlayerUI.instance.UpdateCostPreview(preview.gameObject.activeInHierarchy, price);
+        waters.Clear();
         base.Disable();
     }
 }
---
+++ Assets/WaterdrainStructure.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterdrainStructure : Structure
+{
+    public Root parent;
+    public List<Water> waters = new List<Water>();
+
+    public override void Destroy()
+    {
+        parent?.structures.Remove(this);
+        structures.Remove(this);
+    }
+
+    public override void Setup()
+    {
+        parent?.structures.Add(this);
+        structures.Add(this);
+    }
+
+    public override void Update()
+    {
+        List<Water> drained = new List<Water>();
+        foreach (Water w in waters)
+        {
+            bool d = w.Drain(100);
+            if (d) drained.Add(w);
+        }
+        foreach (Water d in drained)
+        {
+            waters.Remove(d);
+        }
+    }
+}
