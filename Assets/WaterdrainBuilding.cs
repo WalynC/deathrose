907aab0c952dec80e12138237d14ab7a80f926c4
Walyn <walyncurtis@gmail.com>
2022-01-28 19:28:57 -0600

    Begins work on waterdrain building

---
+++ Assets/WaterdrainBuilding.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterdrainBuilding : Building
+{
+    public override void Build()
+    {
+        Debug.Log("build");
+    }
+
+    public override void Preview()
+    {
+        Debug.Log("preview");
+    }
+}
