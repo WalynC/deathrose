737acb75404120b29ade36e2320bbdb590dc7f5b
Walyn <walyncurtis@gmail.com>
2022-01-29 11:07:03 -0600

    Player can now shoot bullets

--- Assets/NightControl.cs
+++ Assets/NightControl.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class NightControl : MonoBehaviour
 {
     public Vector2 lateral = Vector2.zero;
+    public BulletPool pool;
+    public LayerMask targetables;
 
     void Update()
     {
         lateral = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         if (Input.GetKeyDown(KeyCode.Space)) GameState.instance.DaytimeBegin();
         CameraController.instance.transform.position = RoseMovement.instance.transform.position;
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(CameraController.cam.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, targetables))
+            {
+                GameObject obj = pool.GetObject();
+                Bullet bull = obj.GetComponent<Bullet>();
+                obj.transform.position = RoseMovement.instance.transform.position;
+                bull.Setup(null, hit.point, pool);
+            }
+        }
     }
 }
