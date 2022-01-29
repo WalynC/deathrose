0ead9437ace90bb4fb8ac7913c24f1b22c6dfb27
Walyn <walyncurtis@gmail.com>
2022-01-29 14:40:26 -0600

    Adds wood shell/bark barrier structure

---
+++ Assets/Scripts/Woodshell/WoodshellBuilding.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodshellBuilding : Building
+{
+    public MeshRenderer preview;
+    public LayerMask rootLayer;
+    public Material canAfford, noAfford;
+    public GameObject prefab;
+    Root root;
+
+    public override void Build()
+    {
+        if (!buildable) return;
+        GameObject obj = Instantiate(prefab);
+        obj.transform.position = position;
+        WoodshellStructure struc = new WoodshellStructure();
+        struc.parent = root;
+        struc.obj = obj;
+        struc.Setup();
+        PlayerMoney.instance.ChangeMoneyCount(-price);
+    }
+
+    public override void Preview()
+    {
+        buildable = false;
+        preview.gameObject.SetActive(false);
+        RaycastHit rootHit;
+        if (Physics.Raycast(CameraController.cam.ScreenPointToRay(Input.mousePosition), out rootHit, Mathf.Infinity, rootLayer))
+        {
+            position = RootNetwork.instance.GetClosestRootPoint(rootHit.point, out root);
+            preview.gameObject.SetActive(root != null);
+            preview.transform.position = position;
+            if (PlayerMoney.instance.CanAfford(price))
+            {
+                buildable = true;
+                preview.material = canAfford;
+            }
+            else
+            {
+                preview.material = noAfford;
+            }
+        }
+        PlayerUI.instance.UpdateCostPreview(preview.gameObject.activeInHierarchy, price);
+    }
+
+    public override void Disable()
+    {
+        preview.gameObject.SetActive(false);
+        PlayerUI.instance.UpdateCostPreview(preview.gameObject.activeInHierarchy, price);
+        base.Disable();
+    }
+}
---
+++ Assets/Scripts/Woodshell/WoodshellStructure.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodshellStructure : Structure
+{
+    public Root parent;
+    int health = 10;
+
+    public override void DealDamage(Vector3 vec)
+    {
+        if (obj.activeInHierarchy)
+        {
+            health--;
+            if (health <= 0) Destroy();
+        }
+    }
+
+    public override void Destroy()
+    {
+        parent?.structures.Remove(this);
+        structures.Remove(this);
+        obj.SetActive(false);
+    }
+
+    public override void Setup()
+    {
+        parent?.structures.Add(this);
+        structures.Add(this);
+    }
+
+    public override void Update()
+    {
+        //woodshell is just a decoy
+    }
+}
