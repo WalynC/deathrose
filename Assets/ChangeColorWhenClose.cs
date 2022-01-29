c9577cd94f8b52e5ba73cca1efb8602886a0b6db
Walyn <walyncurtis@gmail.com>
2022-01-29 13:39:43 -0600

    Improves visuals, including making flower stem change color when out of range of roots

---
+++ Assets/ChangeColorWhenClose.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorWhenClose : MonoBehaviour
+{
+    public Material dead, alive;
+    public MeshRenderer rend;
+
+    private void Update()
+    {
+        Vector3 plantPos = RootNetwork.instance.GetClosestRootPoint(transform.position);
+        if (Vector3.Distance(plantPos, transform.position) > 5f) rend.material = dead;
+        else rend.material = alive;
+
+
+    }
+}
