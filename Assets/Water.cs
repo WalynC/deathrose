eebacacab97ce8098970e400c3879203d8ecf4d9
Walyn <walyncurtis@gmail.com>
2022-01-28 20:06:48 -0600

    Adds water script to water

---
+++ Assets/Water.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public int capacity = 500;
+    int curCapacity;
+
+    private void Start()
+    {
+        curCapacity = capacity;
+    }
+
+    public void Drain(int amount)
+    {
+        int reward = amount;
+        curCapacity -= amount;
+        bool drained = curCapacity < 0;
+        if (drained) reward += curCapacity;
+        PlayerMoney.instance.ChangeMoneyCount(reward);
+        if (drained) Destroy(gameObject);
+    }
+}
